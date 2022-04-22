8922d7745a8aa06e17e9a4d38a093f045e6c8bc6
Patrick Krämer <patagonahn@gmail.com>
2022-04-22 20:17:22 +0200

    improve config documentation

--- PixelFlutServer/PixelFlutServer.Mjpeg/PixelFlutServerConfig.cs
+++ PixelFlutServer/PixelFlutServer.Mjpeg/PixelFlutServerConfig.cs
@@ -4,14 +4,28 @@
     {
         public int PixelFlutPort { get; set; } = 1234;
         public int MjpegPort { get; set; } = 8080;
+        /// <summary>
+        /// Max FPS to output (lower = less bandwidth)
+        /// </summary>
         public double MaxFps { get; set; } = 60;
         public int Width { get; set; } = 1280;
         public int Height { get; set; } = 720;
+        /// <summary>
+        /// MJPEG quality (lower = less bandwidth)
+        /// </summary>
         public int JpegQualityPercent { get; set; } = 70;
+        /// <summary>
+        /// URI to redirect to when HTTP requests are detected on the pixelflut port
+        /// </summary>
         public string HttpServerUri { get; set; }
+        /// <summary>
+        /// Directory where to save the canvas to so it can be loaded after a restart
+        /// </summary>
         public string PersistPath { get; set; } = ".";
+        /// <summary>
+        /// Text to show in the lower left corner of the canvas
+        /// </summary>
         public string AdditionalText { get; set; }
         public int AdditionalTextSize { get; set; } = 14;
-        public int NetworkBufferSize { get; set; } = 1048576;
     }
 }
